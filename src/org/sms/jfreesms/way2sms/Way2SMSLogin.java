451cef3dbc5980a5cc9c6da4472eef68de3d7edc
Anantha Krishnan S R <ananthakrishnan.s.r@gmail.com>
2012-04-19 15:27:59 +0530

    added nb files

---
+++ src/org/sms/jfreesms/way2sms/Way2SMSLogin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sms.jfreesms.way2sms;
+
+import org.sms.jfreesms.Login;
+
+/**
+ *
+ * @author anantha
+ */
+public class Way2SMSLogin implements Login{
+
+    @Override
+    public void login(String userName, String password) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
